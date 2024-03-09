a9f483dfbff758fb1a411474dc3d4df066ba5532
Miracle Nnabuko <nnabukomiracle@gmail.com>
2024-03-09 06:21:33 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       "source" = "hashicorp/azurerm"
-      version  = ">=3.43.0"
+      version  = ">=3.94.0"
     }
   }
   cloud {
@@ -21,21 +21,21 @@ provider "azurerm" {
 }
 
 resource "random_string" "uniquestring" {
-  length           = 3
-  special          = false
-  upper            = false
+  length  = 3
+  special = false
+  upper   = false
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "811-d134e023-provide-continuous-delivery-with-gith"
-  location = "westus"
+  name     = "813-6d290350-hands-on-with-terraform-on-azure"
+  location = "East US"
 }
 module "storageacc" {
-  source  = "app.terraform.io/mamaafrica/storageacc/azurerm"
-  version = "1.0.0"
-  storage_account_name = "mimistorage${random_string.uniquestring}"
-  resource_group_name = azurerm_resource_group.rg.name
+  source                  = "app.terraform.io/mamaafrica/storageacc/azurerm"
+  version                 = "1.0.0"
+  storage_account_name    = "mimistorage${random_string.uniquestring.result}"
+  resource_group_name     = azurerm_resource_group.rg.name
   resource_group_location = azurerm_resource_group.rg.location
-  Environment = "Production"
+  Environment             = "Production"
 }
 
